a192fccd874efb1ef989c81386e5c9f2d8fba029
Daehan Jang <abouttt57@gmail.com>
2025-12-10 09:56:50 +0900

    fix: Prevent dangling references in DynamicArray::PushBack

--- DynamicArray/DynamicArray.h
+++ DynamicArray/DynamicArray.h
@@ -203,7 +203,16 @@ public:
 
 	void PushBack(const T& value)
 	{
-		EmplaceBack(value);
+		// Prevents dangling references when copying from own elements.
+		if (&value >= m_Data && &value < m_Data + m_Size)
+		{
+			T temp = value;
+			EmplaceBack(std::move(temp));
+		}
+		else
+		{
+			EmplaceBack(value);
+		}
 	}
 
 	void PushBack(T&& value)
